07eb640c6c16645b0b129ff68726c0ebca2c63e2
Titux Metal <github@lgdweb.fr>
2020-03-09 23:42:29 +0100

    Export all components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as withStyled } from './hoc'
+export { default as Home } from './Home'
+export { default as Layout } from './Layout'
+export { default as Login } from './Login'
+export { default as Register } from './Register'
